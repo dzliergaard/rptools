4927620d55dca8f2dcebafa11eff8d6217cce738
Dane Liergaard <dzlier@amazon.com>
2016-08-04 15:49:39 -0700

    Moving backend support classes to their own repository for use by Web and Android applications

---
+++ src/com/rptools/npc/Character.java
@@ -0,0 +1,12 @@
+package com.rptools.npc;
+
+/**
+ * Represents a randomly generated non-player character
+ */
+public class Character {
+
+
+    enum Appearance {
+        DistinctiveJewelry
+    }
+}
